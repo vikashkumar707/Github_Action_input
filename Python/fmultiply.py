5ff5e98a388f52344078c5eb934ce7b674025960
VIKASH KUMAR <mevikash606@gmail.com>
2023-03-14 15:37:12 +0530

    commit

--- Python/fmultiply.py
+++ Python/fmultiply.py
@@ -1,10 +1,10 @@
 import sys
 
-def add(a,b):
+def multiply(a,b):
     return a*b
 
 
 
 a=int(sys.argv[1])
 b=int(sys.argv[2])
-c=print(f'The sum of {a} and {b} is:',add(a,b))
+c=print(f'The sum of {a} and {b} is:',multiply(a,b))
