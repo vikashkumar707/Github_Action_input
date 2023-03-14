3c166878298268569ff8ba71906d6a1b3ea3bb99
VIKASH KUMAR <mevikash606@gmail.com>
2023-03-14 15:28:29 +0530

    commit

--- Python/fmultiply.py
+++ Python/fmultiply.py
@@ -1,6 +1,9 @@
 import sys
 
+def multiply(num1,num2):
+    return num1*num2
+
 num1=int(sys.argv[1])
 num2=int(sys.argv[2])
-result = num1 * num2
-print("The product of", num1, "and", num2, "is", result)
+
+c=print(f'The multiply of {num1} and {num2} is:',multiply(num1,num2))
